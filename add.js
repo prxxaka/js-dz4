3a62230fd245e7ee60af64abbdde88b60d3fc5ef
prxxaka <sooronbaeva03@gmail.com>
2022-12-14 00:31:55 +0600

    js-dz2

---
+++ add.js
@@ -0,0 +1,16 @@
+for (var i = 2; i <= 20; i++){
+    if ( i % 2 === 0){
+        console.log(i)
+    }
+}
+
+
+
+var sum = 0
+for (var i = 0; i > 0; i++){
+    sum = sum + i
+}
+do {
+    i = prompt('введите число')
+} while (i > 0)
+console.log('сумма чисел - ', sum)
